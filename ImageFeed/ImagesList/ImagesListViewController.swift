775147400228e850ec0e369d1a8560592a617887
smirnov_mm <sradio09@yandex.ru>
2025-06-15 19:48:32 +0300

    Выполнение задания

--- ImageFeed/ImagesList/ImagesListViewController.swift
+++ ImageFeed/ImagesList/ImagesListViewController.swift
@@ -5,14 +5,15 @@ final class ImagesListViewController: UIViewController {
     @IBOutlet private var tableView: UITableView!
     
     private let photosName: [String] = Array(0..<20).map{ "\($0)" }
+    private let showSingleImageSegueIdentifier = "ShowSingleImage"
     
     private lazy var dateFormatter: DateFormatter = {
-            let formatter = DateFormatter()
-            formatter.dateStyle = .long
-            formatter.timeStyle = .none
+        let formatter = DateFormatter()
+        formatter.dateStyle = .long
+        formatter.timeStyle = .none
         formatter.locale = Locale(identifier: "ru_RU")
-            return formatter
-        }()
+        return formatter
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +22,21 @@ final class ImagesListViewController: UIViewController {
         tableView.contentInset = UIEdgeInsets(top: 12, left: 0, bottom: 12, right: 0)
         tableView.dataSource = self
         tableView.delegate = self
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == showSingleImageSegueIdentifier {
+            guard
+                let viewController = segue.destination as? SingleImageViewController,
+                let indexPath = sender as? IndexPath
+            else {
+                super.prepare(for: segue, sender: sender)
+                return
+            }
+            let image = UIImage(named: photosName[indexPath.row])
+            viewController.image = image
+        } else {
+            super.prepare(for: segue, sender: sender)
+        }
     }
     
     private func configCell(for cell: ImagesListCell, at indexPath: IndexPath) {
@@ -44,16 +60,19 @@ extension ImagesListViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         let imageName = photosName[indexPath.row]
         guard let image = UIImage(named: imageName) else {
-                    return 200
-                }
-                
-                let imageSize = image.size
-                let imageViewWidth = tableView.bounds.width
-                let scale = imageViewWidth / imageSize.width
-                let imageViewHeight = imageSize.height * scale
-                
-                let topBottomInset: CGFloat = 24
-                return imageViewHeight + topBottomInset
+            return 200
+        }
+
+        let imageSize = image.size
+        let imageViewWidth = tableView.bounds.width
+        let scale = imageViewWidth / imageSize.width
+        let imageViewHeight = imageSize.height * scale
+
+        let topBottomInset: CGFloat = 24
+        return imageViewHeight + topBottomInset
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: showSingleImageSegueIdentifier, sender: indexPath)
     }
 }
 
